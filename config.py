6df1cca55a95d5a05cea45f2a371bfc21b72a1e5
Sugiyama <you.sunabaco@gmail.com>
2026-02-23 13:33:24 +0900

    ログイン状態を保持するように調整

--- config.py
+++ config.py
@@ -1,7 +1,10 @@
 import os
+from datetime import timedelta
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(os.path.abspath(os.path.dirname(__file__)), 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # 「ログイン状態を保持する」を有効にした場合のクッキー有効期限（1週間）
+    REMEMBER_COOKIE_DURATION = timedelta(days=7)
